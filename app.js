2ae8ce2d2dbb25c591d75af99e0e137f5a4bb80c
Gagandeep Kang <gskang@bu.edu>
2019-11-23 12:59:04 -0500

    started projects template

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.get("/", function(req, res){
 });
 
 
+app.get("/about/", function(req,res){
+    res.render("about.ejs")
+});
+
+app.get("/projects/", function(req,res){
+    res.render("projects.ejs")
+});
+
+
 
 app.listen(3000, function(){
     console.log("Personal website is listening on Port 3000"); 
