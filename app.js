90be4cfbecdff841367572b93719ac1f000da887
Gagandeep Kang <gskang@bu.edu>
2019-12-01 01:14:20 -0500

    finished adding scrolly button to all pages

--- app.js
+++ app.js
@@ -18,11 +18,27 @@ app.get("/projects/", function(req,res){
     res.render("projects.ejs")
 });
 
+ /* -----Projects Routes ------ */ 
 app.get("/fakenews/", function(req,res){
     res.render("fakenews.ejs")
 });
 
+app.get("/agriworks/", function(req,res){
+    res.render("agriworks.ejs")
+});
 
+app.get("/yelpcamp/", function(req,res){
+    res.render("yelpcamp.ejs")
+});
+app.get("/silentparty/", function(req,res){
+    res.render("silentparty.ejs")
+});
+app.get("/muchtodo/", function(req,res){
+    res.render("muchtodo.ejs")
+});
+app.get("/quickstop/", function(req,res){
+    res.render("quickstop.ejs")
+});
 
 app.listen(3000, function(){
     console.log("Personal website is listening on Port 3000"); 
